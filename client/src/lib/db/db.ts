70f74cab6d4e3c8dad0128a3aa3af658e36266a0
Aditya Sahu <aditya.sahu@sprinklr.com>
2024-07-10 21:43:45 +0530

    shifted data inside app

--- client/src/lib/db/db.ts
+++ client/src/lib/db/db.ts
@@ -8,7 +8,7 @@ const reportsRoot = config.reportsPath;
 
 export async function getPath(username: string, pathType: PathType) {
   if (pathType === "report") {
-    return `/reports/${username}.json`;
+    return path.join(process.cwd(), "/app/report/", `${username}.json`);
   }
   if (pathType === "user")
     return path.join(process.cwd(), reportsRoot, `${username}`);
