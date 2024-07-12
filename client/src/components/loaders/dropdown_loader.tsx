04d05308a2653f314ef6ae4fa2938b6504c67121
Aditya Sahu <aditya.sahu@sprinklr.com>
2024-07-12 13:49:46 +0530

    deleted loader

--- client/src/components/loaders/dropdown_loader.tsx
+++
@@ -1,9 +0,0 @@
-import ContentLoader from "react-content-loader";
-
-export function DropdownLoader() {
-  return (
-    <ContentLoader viewBox="0 0 400 100">
-      <rect x="80" y="17" rx="4" ry="4" width="300" height="13" />
-    </ContentLoader>
-  );
-}
